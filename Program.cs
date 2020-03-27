b92823beee0189e0e7e24e07a30c2c3a941a40c2
unknown <aminmirzoev123@gmail.com>
2020-03-27 23:47:13 +0500

    updadate

--- Program.cs
+++ Program.cs
@@ -19,72 +19,68 @@ namespace HW_4
                 }
                 System.Console.WriteLine($"Sale={sale}");*/
 
-               /* int a=3,b=83,c=463,d=43;
-                   int count=0;
-                    if(a!=b&&b!=c&&c!=d)
-                    {
-                        if(a<c&&a<b&&a<d)
-                        {
-                            System.Console.WriteLine(a);
-                        }
-                        if(b<c&&b<d&&b<a)
-                        {
-                            System.Console.WriteLine(b);
-                        }
-                        if(c<a&&c<b&&c<d)
-                        {
-                            System.Console.WriteLine(c);
-                        }
-                        if(d<c&&d<b&&d<a)
-                        {
-                            System.Console.WriteLine(d);
-                        }
-                    }
-                    else if(a==b&&b==c&&c==d)
-                    {
-                       count= a*b*c*d;
-                       System.Console.WriteLine(count);
-                    }
-                    else if(a>b&&b>c&&c>d)
-                    {
-                        System.Console.WriteLine("Числа расположены по возрастанию");
-                    }*/
+                    int a= 2,b=2,c=2,d=5;
+                    int Count=0;
+                      if(a==b&&b==c&&c==d)
+                     {
+                         Count=a*b*c*d;
+                         System.Console.WriteLine($"Count={Count}");
+                     }
+                    else if(a<=b&&b<=c&&c<=d)
+                     {
+                         System.Console.WriteLine("«Числа расположены по возрастанию».");
+                     }
+                     else if(a<=d&&a<=b&&a<=c)System.Console.WriteLine(a);
+                     else if(b<=a&&b<=c&&b<=d)System.Console.WriteLine(b);
+                     else if(c<=a&&c<=b&&c<=d)System.Console.WriteLine(c);
+                     else if(d<=a&&d<=c&&d<=b)System.Console.WriteLine(d);
+                     
 
-                    int a=5,b=4,c=9,w=0;
+
+                  /*  int a=10,b=9,c=10,w=0;
                     if(a>=b&&b>=c)
                     {
-                        System.Console.WriteLine($"A={a}B={b}C={c}");
+                        System.Console.WriteLine($"A={a} B={b} C={c}");
                     }
                     else if(a<=b&&b<=c)
                     {
                         w=a;
                         a=c;
                         c=w;
-                        System.Console.WriteLine($"A={a}B={b}C={c}");
+                        System.Console.WriteLine($"A={a} B={b} C={c}");
                     }
                     else if(b>=a&&a>=c)
                     {
                         w=a;
                         a=b;
                         b=w;
-                        System.Console.WriteLine($"A={a}B={b}C={c}");
+                        System.Console.WriteLine($"A={a} B={b} C={c}");
                     }
                     else if(a>=c&&c>=b)
                     {
                         w=b;
                         b=c;
                         c=w;
-                        System.Console.WriteLine($"A={a}B={b}C={c}");
+                        System.Console.WriteLine($"A={a} B={b} C={c}");
                     }
                     else if(c>=b&&a>=b)
                     {
                         w=b;
-                        c=a;
-                        w=c;
+                        b=a;
+                        a=c;
+                        c=w;
+
+
+                        System.Console.WriteLine($"A={a} B={b} C={c}");
                     }
                     else
-                    {System.Console.WriteLine("Этот вариант не был добавнел");}
+                    {System.Console.WriteLine("Этот вариант не был добавнел");}*/
               Console.ReadKey();
+                     
+                         
+      
+
+    
         }
     }
 }
